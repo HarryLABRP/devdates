716c669c0af84a44fb9c519a090dc4731e7d3ded
HarryLABRP <harryboray@gmail.com>
2021-05-04 18:11:49 +0100

    updated

---
+++ script/config.php
@@ -0,0 +1,59 @@
+<?php
+
+  // THAT'S THE CURRENT CONFIGURATION
+  // V1.2
+  // MADE BY CATALIN
+  
+  $title_of_page = "Laborious Roleplay"; // Change title of the page
+  $description_of_page = "An amazing server!"; // Change the description of your website
+  $ip_server = "eu.hypixel.net"; // Change the ip
+  $port_server = "25565"; // Change the port
+  $footer_copyright = "© 2020 Example All Rights Reserved."; // Footer copyright text
+  $particle_color = "#ffffff";
+  $enable_copyright_trademark = true;
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  $raspuns_copyright = "";
+
+  if ($enable_copyright_trademark) {
+    $raspuns_copyright = "block";
+  } else {
+    $raspuns_copyright = "none";
+  }
+
+?>
